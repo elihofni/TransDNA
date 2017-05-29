24f696a74f0bce8a1468953ca52bbbfa879fc1c8
Max Fratane <mfratane@id.uff.br>
2017-05-29 19:42:49 -0300

    split

---
+++ cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_b1aff.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_b1aff.
+# This may be replaced when dependencies are built.
---
+++ cmake-build-debug/CMakeFiles/TransDNA.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/Arquivos/Documents/GitHub/TransDNA/functions.c" "D:/Arquivos/Documents/GitHub/TransDNA/cmake-build-debug/CMakeFiles/TransDNA.dir/functions.c.obj"
+  "D:/Arquivos/Documents/GitHub/TransDNA/merge.c" "D:/Arquivos/Documents/GitHub/TransDNA/cmake-build-debug/CMakeFiles/TransDNA.dir/merge.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
