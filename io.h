0e9f6c19b695695f30629a4c9ea0166dd9d806d2
Eli <elihofnil@id.uff.br>
2017-06-07 02:23:36 -0300

    Troca de mensagens funcionando

---
+++ io.h
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+
+char* ler(char* caminho);
+void escrever(char* str, char* caminho);
--- transcription.h
+++ transcription.h
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-
 char* transcription(char *chain,int size);
 void initialize(char *array,int size);
-const char* aminoacids(char *in,const int size);
-//mantive o parâmetro size, mesmo sabendo que o tamanho é fixo em 3 para desencargo de consiência
+char* aminoacids(char *in,const int size);
+char** split(char* str, int subStringSize);
